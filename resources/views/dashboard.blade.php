0b83f36246076c6c5c7c3508a6b4ba3f43e49608
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2023-10-25 15:08:45 -0500

    chore: laravel lang

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-gray-200" >
-                <h1 class="text-4xl text-center">BIENVENIDOS A XUE TEJIDOS</h1>
+                <h1 class="text-4xl py-5 text-center">BIENVENIDOS A XUE TEJIDOS</h1>
             </div>
         </div>
     </div>
