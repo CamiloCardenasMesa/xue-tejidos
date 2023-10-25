bf1aa1f84abf2c906787a8b28183a67096819ac4
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2023-10-25 17:36:40 -0500

    feat: model, migrations, factory, seeder of post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Camilo Mesa',
+            'email' => 'camilo@xue-tejidos.com',
+        ]);
+
+        Post::factory(100)->create();
     }
 }
