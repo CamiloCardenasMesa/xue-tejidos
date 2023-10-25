ae182b52f63c02640fdaed8d7b1d9f8a8869c62d
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2023-10-25 17:36:59 -0500

    feat: show post on dashboard view

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,7 @@
+<div class="my-2 overflow-x-auto px-4 sm:px-6 lg:px-8">
+    <div class="py-2 align-middle inline-block min-w-full">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            {{ $slot }}
+        </div>
+    </div>
+</div>
