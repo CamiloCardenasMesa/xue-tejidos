80d2d7c1845e9194d2baea0418f316a784d87da4
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2023-10-27 22:23:20 -0500

    style: auth logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,24 @@
         </h2>
     </x-slot>
 
-    <div class="py-9">
-        <div class="max-w-7xl mx-auto">
-            <div class="my-2 overflow-x-auto px-4 sm:px-6 lg:px-8">
-                <div class="flex h-36 bg-gray-300 items-center justify-center">
-                    <h3>
-                        aquí van los productos de xue
-                    </h3>
-                </div>
-            </div>
+    <div class="flex">
+        <img class="w-full" src="{{ asset('storage/posts/portada.jpg') }}" alt="" >
+    </div>
+    <div class="flex h-96 py-16 bg-gray-200 justify-around">
+        <div class="p-3 text-2xl font-bold">
+            <h1>Hombre</h1>
+            <img src="{{ asset('storage/posts/hombre.jpg') }}" alt="">
         </div>
+        <div class="p-3 text-2xl font-bold">
+            <h1>Mujer</h1>
+            <img src="{{ asset('storage/posts/mujer.jpg') }}" alt="">
+        </div>
+        <div class="p-3 text-2xl font-bold">
+            <h1>Niños</h1>
+            <img src="{{ asset('storage/posts/niños.jpg') }}" alt="">
+        </div>
+    </div>
+    <div>
+        <div class="w-full h-60 bg-red-400"></div>
     </div>
 </x-app-layout>
