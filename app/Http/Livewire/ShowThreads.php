dac92d80a45447cd28e6a479328a9331af07f820
Camilo <arqcamilocardenasmesa@gmail.com>
2023-11-17 16:40:51 -0500

    feat: seach bar

--- app/Http/Livewire/ShowThreads.php
+++ app/Http/Livewire/ShowThreads.php
@@ -8,14 +8,19 @@ use Livewire\Component;
 
 class ShowThreads extends Component
 {
+    public $search = '';
+    
     public function render()
     {
         $categories = ForumCategory::get();
-        $threads = Thread::latest()->withCount('replies')->get();
+        $threads = Thread::query();
+        $threads->where('title', 'like', '%'.$this->search.'%');
+        $threads->withCount('replies');
+        $threads->latest();
 
         return view('livewire.show-threads', [
             'categories' => $categories,
-            'threads' => $threads,
+            'threads' => $threads->get(),
         ]);
     }
 }
