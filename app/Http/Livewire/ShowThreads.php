998f4dd15c393a460c70b5e5911da61ec5793fbf
Camilo <arqcamilocardenasmesa@gmail.com>
2023-11-17 15:40:46 -0500

    feat: visual structure of the questions

--- app/Http/Livewire/ShowThreads.php
+++ app/Http/Livewire/ShowThreads.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\ForumCategory;
+use App\Models\Thread;
 use Livewire\Component;
 
 class ShowThreads extends Component
@@ -10,8 +11,11 @@ class ShowThreads extends Component
     public function render()
     {
         $categories = ForumCategory::get();
+        $threads = Thread::latest()->get();
+
         return view('livewire.show-threads', [
             'categories' => $categories,
+            'threads' => $threads,
         ]);
     }
 }
