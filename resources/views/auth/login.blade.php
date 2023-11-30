8e69ff8528a9b69182d933727cf755aa95f5037d
Camilo <arqcamilocardenasmesa@gmail.com>
2023-11-30 18:42:16 -0500

    feat: register option added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,24 +25,31 @@
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
+            <div class="flex justify-between mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
+            </div>
 
-                <x-button class="ml-4">
+            <div class="flex items-center justify-end my-6">
+                <x-button class="flex-1 justify-center">
                     {{ __('Log in') }}
                 </x-button>
             </div>
         </form>
+        <div class="flex justify-center text-sm pb-3">
+            <a href="{{ route('register') }}">
+                <span>
+                    <span>¿No tienes cuenta?</span>
+                    <span class="text font-bold ml-1">Regístrate</span>
+                </span>
+            </a>
+        </div>
     </x-authentication-card>
 </x-guest-layout>
