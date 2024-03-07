d68202cfca9c303ecd0725ba53094fed9c9063c9
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2024-03-07 10:12:25 -0500

    feat: welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,22 @@
-<x-app-layout>
-    <div class="text-center text-4xl">
-        En desarrollo
-    </div>
-</x-app-layout>
+<x-guest-layout>
+    @include('navigation-menu')
+    <header class="h-screen">
+        <div class="relative flex justify-center items-center">
+            <div class="h-screen">
+                <img class="cover opacity-5" src="{{ asset('storage/images/guest/header.jpg') }}" alt="gorros tejidos">
+            </div>
+            <x-button class="absolute">
+                Accede a los descuentos
+            </x-button>
+        </div>
+    </header>
+    <main>
+        <section>
+            <div class="grid grid-cols-3 gap-1 my-3">
+                <div class="bg-red-200 h-[728px]"></div>
+                <div class="bg-green-300 h-[728px]"></div>
+                <div class="bg-blue-500 h-[728px]"></div>
+            </div>
+        </section>
+    </main>
+</x-guest-layout>
