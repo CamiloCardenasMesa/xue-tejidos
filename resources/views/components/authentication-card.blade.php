8f3facc12443e687a265cbeca227dd8becd5548c
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2024-03-08 17:53:16 -0500

    style: responsive design in login and registration views

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,7 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
-    </div>
-
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+<div class="flex flex-grow items-center justify-center">
+    <div class="flex flex-col justify-center items-center w-screen md:w-max lg:w-max">
+        <div class="w-full lg:my-6 px-9 py-6 bg-white shadow-md overflow-hidden">
+            {{ $slot }}
+        </div>
     </div>
 </div>
