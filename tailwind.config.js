faede674cf58a591dba16618cac11981d789d9bc
Camilo <95107808+CamiloCardenasMesa@users.noreply.github.com>
2024-03-08 17:47:32 -0500

    style: font roboto added

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                roboto: ['Roboto', 'sans-serif'],
             },
         },
     },
